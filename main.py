0590786198ea74b732bde592c6b9cbcf00093bab
ztgillette <ztgillette@gmail.com>
2025-02-09 10:20:54 -0600

    main

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ from bitboard.knights import *
 
 def main():
 
-    sample_blockers = Bitboard(0x0000000000000000)
+    other_blockers = Bitboard(0x0000000000000000)
     #bitboard.print(sample_blockers.bitboard, "blockers")
+
+    white_pawns = Pawns(WHITE)
+    black_pawns = Pawns(BLACK)
     
     white_knights = Knights(WHITE)
     black_knights = Knights(BLACK)
 
-    Bitboard.print(white_knights.bitboard | black_knights.bitboard)
+    white_king = Kings(WHITE)
+    black_king = Kings(BLACK)
 
-    empty_squares = Bitboard.complement(sample_blockers.bitboard)
+    all_pieces = other_blockers.bitboard | white_pawns.bitboard | black_pawns.bitboard | white_knights.bitboard | black_knights.bitboard | white_king.bitboard | black_king.bitboard
+
+    Bitboard.print(all_pieces)
+
+    empty_squares = Bitboard.complement(all_pieces)
     
     Bitboard.print(black_knights.all_moves(empty_squares), "after")
 
